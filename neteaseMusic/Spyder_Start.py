fa95abf2d079efbdbf60638be7115d1bc671ed4c
swiich <582326423@qq.com>
2017-11-14 09:44:28 +0800

    Revert "ss"
    This reverts commit 5e73cac0b02a2c240f189e6401ee5495c54ea1ba.

--- neteaseMusic/Spyder_Start.py
+++ neteaseMusic/Spyder_Start.py
@@ -4,7 +4,7 @@ import GetPlaylistByUserId
 if __name__ == '__main__':
     total = 0
     # 5832286  28 `  34885501  118    108781179  41   46543242  177   44590287 473  41299404 202   3428554  1-0   29372996 932  29631520 1001  7004245  864
-    playlist_id = GetPlaylistByUserId.GetPlaylistID_All('5832286')
+    playlist_id = GetPlaylistByUserId.GetPlaylistID_All('29631520')
     for i in playlist_id:
         songInfo = GetSongsByPlaylist.GetSongsInfo(str(i))
         for k, v in songInfo.items():
